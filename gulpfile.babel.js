c360995cea2cbe7f260e2fb2749e3bfbd210a30f
Renziito <renzo.aliaga@upch.pe>
2017-10-11 18:30:56 -0500

    base changes

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,15 +1,8 @@
 import gulp from 'gulp';
 import phpmin from 'gulp-php-minify';
-import shell from 'gulp-shell';
 import browserSync from 'browser-sync';
 
 let bsync = browserSync.create();
-//let reload = browserSync.reload;
-
-gulp.task('yii-pathreq', shell.task([
-    'if [ ! -d "dist/runtime" ]; then\nmkdir dist/runtime\nchmod 777 -R dist/runtime\nfi',
-    'if [ ! -d "dist/assets" ]; then\nmkdir dist/assets\nchmod 777 -R dist/assets\nfi',
-]));
 
 gulp.task('php', (cb) => {
     return gulp.src(['./src/backend/**/*.php'])
@@ -24,7 +17,7 @@ gulp.task('php-watch', ["php"], (done) => {
 
 gulp.task('server',["php"],() => {
     bsync.init({
-        proxy: "localhost/v3/padlock/dist",
+        proxy: "localhost/v3/upch-padlock/dist",
     });
 
     gulp.watch(['./src/backend/**/*.php'], ['php-watch']);
--- src/backend/views/site/index.php
+++ src/backend/views/site/index.php
@@ -20,10 +20,7 @@ $this->title = 'My Yii Application';
             <div class="col-lg-4">
                 <h2>Heading</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>Hola mama no quiero ir a estudiiar. Porfavor no me obligues</p>
 
                 <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
             </div>
