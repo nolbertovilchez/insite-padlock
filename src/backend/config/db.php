d5a8172fabdeaea01f44059361e279d652d5cb71
Francisco Isasi <francisco.isasi@upch.pe>
2017-11-23 16:04:33 -0500

    Modulo application, pendiente js

--- src/backend/config/db.php
+++ src/backend/config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'dblib:host=upchdbtest;dbname=PADLOCK',
+    'username' => 'sachacad',
+    'password' => 'b4A15E56cB959F',
     'charset' => 'utf8',
 ];
